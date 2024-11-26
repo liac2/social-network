1d37cf95e6e9410377581da581d66385e8092e20
liac2 <lukas.altenriederer@gmail.com>
2024-11-26 17:15:54 +0000

    add page funct and begin search()

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.page, name="page"),
+    path("wiki/", views.wiki, name="wiki"),             # wiki/ path without argument - only for using as url
+    path("search/", views.search, name="search")
 ]

3f8e2a13737deecb256276701aa96b8f244afa07
liac2 <lukas.altenriederer@gmail.com>
2024-11-28 10:18:50 +0000

    new()

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,6 +6,8 @@ app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.page, name="page"),
+    path("wiki/<str:title>/edit", views.edit, name="edit"),
     path("wiki/", views.wiki, name="wiki"),             # wiki/ path without argument - only for using as url
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("new/", views.new, name="new")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,39 +8,77 @@ from . import util
 class SearchPage(forms.Form):
     search = forms.CharField()
 
+class NewPage(forms.Form):
+    title = forms.CharField(label="Enter a title")
+    content = forms.CharField(widget=forms.Textarea, label="Enter content")
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(), "form": SearchPage(), "heading": "All Pages"
     })
 
 
+# Creating new wiki page
+def new(request):
+    if request.method == "POST":
+        data = NewPage(request.POST)
+
+        if data.is_valid():
+            title = data.cleaned_data["title"]
+            content = data.cleaned_data["content"]
+            if util.get_entry(title) != None:
+                return render(request, "encyclopedia/error.html", {
+                    "error": "Page already exists" 
+                })
+            util.save_entry(title=title, content=content)
+            return HttpResponseRedirect(reverse('wiki:wiki') + title)
+
+        # If search invalid: let user try again
+        else:
+            return render(request, "encyclopedia/new.html", {
+                "form": SearchPage(),"new_page_form": data, "heading": "Create new page"
+            })
+
+
+    else:
+        return render(request, "encyclopedia/new.html", {
+            "form": SearchPage() ,"new_page_form": NewPage(), "heading": "Create new page"
+        })
+
 def search(request): 
     # Search for page
     if request.method == "POST":
         search = SearchPage(request.POST)
 
+        # If valid input
         if search.is_valid():
-            page = search.cleaned_data["search"]
-            content = util.get_entry(page)
+            title = search.cleaned_data["search"]
+            content = util.get_entry(title)
 
             # If found: visit page
             if content != None:
                 return render(request, "encyclopedia/page.html", {
-                    "title": page, "content": content
+                    "title": title, "form": SearchPage(), "content": content
                 })
 
             # Else: show page titles with have query as substring 
             else: 
+                pages = util.list_entries()
+                results = []
+                for page in pages:
+                    if title in page:
+                        results.append(page)
                 return render(request, "encyclopedia/index.html", {
-                    "entries": , "form": SearchPage(), "heading": "Page not Found", "message": "Similar Pages" 
+                    "entries": results, "form": search, "heading": "Page not Found", "message": "Similar Pages" 
                 })
             
-        # If search invalid: error
+        # If search invalid: let user try again
         else:
-            return render(request, "encyclopedia/error.html", {
-                "entries": , "form": SearchPage(), "heading": "Page not Found", "message": "Similar Pages" 
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries(), "form": search, "heading": "All Pages", "message": "" 
             })
 
+    # If GET
     else:
         return
     
@@ -51,6 +89,10 @@ def search(request):
 def wiki(request):
     pass 
 
+
+# Edits a page
+def edit(request, title):
+    pass
 
 def page(request, title):
 
@@ -65,7 +107,7 @@ def page(request, title):
     
     # set <title> to the var title 
     return render(request, "encyclopedia/page.html", {
-        "title": title, "content": data
+        "title": title, "form": SearchPage(), "content": data
     })
     
 

a693df65004bf47d81097fee6ccff48c32757afd
liac2 <lukas.altenriederer@gmail.com>
2025-01-15 09:19:40 +0000

    all posts front-end

--- project4/network/static/network/main.js
+++ project4/network/static/network/main.js
@@ -22,7 +22,24 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('/post')
     .then(response => response.json())
     .then(posts => {
-        for post in posts
+
+        // List all posts
+        for (let data of posts) {
+            let post = document.createElement('a');
+            post.className = 'list-group-item list-group-item-action list-group-item-light bg-light';
+            post.dataset.id = data.id;
+            post.href = '#';
+            post.ariaCurrent = 'true';
+
+            post.innerHTML = `<div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">${data.creator}</h5>
+            <small>${data.time}</small>
+            </div>
+            <p class="mb-1">${data.text}</p>
+            <p class="mb-1">${data.likes}</p>`;
+
+            view.append(post);
+        }
 
     });
 });

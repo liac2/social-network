0eb109d5d1b5c666601f042ce265ae1cd8b18737
liac2 <lukas.altenriederer@gmail.com>
2025-01-29 08:50:30 +0000

    sth

--- project4/network/serializers.py
+++ project4/network/serializers.py
@@ -3,18 +3,17 @@ from .models import User, Post
 
 
 class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(source="followers.count", read_only=True)
+    following_count = serializers.IntegerField(source="following.count", read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'email']
-
+        fields = ["id", "username", "email", "followers_count", "following_count"]
 
 class PostSerializer(serializers.ModelSerializer):
+    creator = serializers.ReadOnlyField(source="user.username")
+    likes_count = serializers.IntegerField(source="users_liked.count", read_only=True)
+
     class Meta:
         model = Post
-        fields = [
-            "id",
-            "user",
-            "text",
-            "time",
-            "users_liked"
-        ]
+        fields = ["id", "creator", "text", "time", "likes_count"]
